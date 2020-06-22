faf523618a6a2560090fc423222b9db80984bb7a
guangyong.lgy <guangyong.lgy@alibaba-inc.com>
2020-06-22 12:58:29 +0800

    change for build tg7100b and ch6121evb

--- board/ch6121evb/script/genbin.sh
+++ board/ch6121evb/script/genbin.sh
@@ -2,6 +2,14 @@
 WORK_PATH=$1
 ELF_FILE=$2
 rm -f $WORK_PATH/*.hex
-arm-none-eabi-objcopy -j .text -j .rodata -j .ARM.exidx -O binary $ELF_FILE $WORK_PATH/xprim
-arm-none-eabi-objcopy -j .data_text -j .data -O binary $ELF_FILE $WORK_PATH/prim
-arm-none-eabi-objcopy -j .jmp_table -O binary $ELF_FILE $WORK_PATH/jumptb
+#arm-none-eabi-objcopy -j .text -j .rodata -j .ARM.exidx -O binary $ELF_FILE $WORK_PATH/xprim
+#arm-none-eabi-objcopy -j .data_text -j .data -O binary $ELF_FILE $WORK_PATH/prim
+#arm-none-eabi-objcopy -j .jmp_table -O binary $ELF_FILE $WORK_PATH/jumptb
+
+
+TOPDIR=`pwd`
+TOOLCHAIN_DIR=$TOPDIR/build/compiler/gcc-arm-none-eabi/Linux64/bin
+$TOOLCHAIN_DIR/arm-none-eabi-objcopy -j .text -j .rodata -j .ARM.exidx -O binary $ELF_FILE $WORK_PATH/xprim
+$TOOLCHAIN_DIR/arm-none-eabi-objcopy -j .data_text -j .data -O binary $ELF_FILE $WORK_PATH/prim
+$TOOLCHAIN_DIR/arm-none-eabi-objcopy -j .jmp_table -O binary $ELF_FILE $WORK_PATH/jumptb
+
